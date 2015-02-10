122faf0837ca8e7031e7f392562d6a5b2f908b98
Cloud <cloud@txthinking.com>
2015-02-10 18:33:24 +0800

    update hosts

--- tools/dnsmasq.sh
+++ tools/dnsmasq.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat ../scripts/hosts.all | awk '{print "address=/"$2"/"$1}' > ../google.dnsmasq.conf
+cat ../scripts/hosts.all | awk '{if(NF==2){ print "address=/"$2"/"$1}}' > ../google.dnsmasq.conf
