2b4960330f0381ba7b6ab8033cdbfe8588d89d41
Cloud <cloud@txthinking.com>
2015-03-06 14:41:13 +0800

    add multi process support

--- scripts/getssl.sh
+++ scripts/getssl.sh
@@ -22,39 +22,60 @@ if [ ! -d output ]
 then
     mkdir output
 fi
+
+max_process=100
+
+fd=/tmp/google-hosts.fd
+mkfifo $fd
+exec 9<>$fd
+rm $fd
+for((i=0;i<$max_process;i++))
+do
+    echo
+done >&9
+
 output=output/$1.x
 > $output
 echo -e "IP\tLOSS\tTIME\tSSL"
 for((i=0;i<255;i++))
 do
-    ip=${1}.${i}
-    c=$(nmap --host-timeout 2s $ip -p 443 2>/dev/null | grep -Pc "443/tcp open")
-    if [ $c -ne 1 ]
-    then
-        echo -e "$ip\tNO\tNO\tNO"
-        echo -e "$ip\tNO\tNO\tNO" >> $output
-        continue
-    fi
-    cer=$(curl https://$ip 2>&1 | grep -Po "'\S*'" |head -1|cut -d \' -f 2)
-    if [ -z $cer ]
-    then
-        echo -e "$ip\tNO\tNO\tNO"
-        echo -e "$ip\tNO\tNO\tNO" >> $output
-        continue
-    fi
-    ping=/tmp/ping-$ip
-    ping -c 5 -w 5 $ip > $ping
-    loss=$(grep -Po "\w+%" $ping)
-    c=$(grep -c "time=" $ping)
-    if [ $c -eq 0 ]
-    then
-        echo -e "$ip\t$loss\tNO\t$cer"
-        echo -e "$ip\t$loss\tNO\t$cer" >> $output
-        continue
-    fi
-    avgtime=$(grep -P "time=" $ping | awk '{print $7}' | awk 'BEGIN {FS="=";s=0;c=0;}{s+=$2;c++;} END {print s/c}')
-    echo -e "$ip\t$loss\t$avgtime\t$cer"
-    echo -e "$ip\t$loss\t$avgtime\t$cer" >> $output
+    {
+        read -u9
+        {
+            ip=${1}.${i}
+            c=$(nmap --host-timeout 2s $ip -p 443 2>/dev/null | grep -Pc "443/tcp open")
+            if [ $c -ne 1 ]
+            then
+                echo -e "$ip\tNO\tNO\tNO"
+                echo -e "$ip\tNO\tNO\tNO" >> $output
+                continue
+            fi
+            cer=$(curl https://$ip 2>&1 | grep -Po "'\S*'" |head -1|cut -d \' -f 2)
+            if [ -z $cer ]
+            then
+                echo -e "$ip\tNO\tNO\tNO"
+                echo -e "$ip\tNO\tNO\tNO" >> $output
+                continue
+            fi
+            ping=/tmp/ping-$ip
+            ping -c 5 -w 5 $ip > $ping
+            loss=$(grep -Po "\w+%" $ping)
+            c=$(grep -c "time=" $ping)
+            if [ $c -eq 0 ]
+            then
+                echo -e "$ip\t$loss\tNO\t$cer"
+                echo -e "$ip\t$loss\tNO\t$cer" >> $output
+                continue
+            fi
+            avgtime=$(grep -P "time=" $ping | awk '{print $7}' | awk 'BEGIN {FS="=";s=0;c=0;}{s+=$2;c++;} END {print s/c}')
+            echo -e "$ip\t$loss\t$avgtime\t$cer"
+            echo -e "$ip\t$loss\t$avgtime\t$cer" >> $output
+            echo >&9
+        }
+    }&
 done
+wait
+exec 9>&-
+
 sort -k4 -k2n -k3n $output -o $output
 echo "[INFO] Done in $output"
