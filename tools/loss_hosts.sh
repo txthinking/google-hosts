3eb4dedd4756de42698aa2e0c334e5807712b7de
Cloud <cloud@txthinking.com>
2015-04-21 12:18:58 +0000

    add more hosts

--- tools/loss_hosts.sh
+++ tools/loss_hosts.sh
@@ -10,7 +10,7 @@
 if [ $# -ne 2 ]
 then
     echo -e "Usage:\n";
-    echo -e "    $ ./loss_hosts.sh hosts_a hosts_b
+    echo -e "    $ ./loss_hosts.sh hosts_a hosts_b\n"
     echo -e "\nView loss_hosts.sh file to see more.\n"
     exit 0;
 fi
@@ -19,3 +19,4 @@ b=_b
 cat $1 | awk '{print $2}' | sort | uniq > $a
 cat $2 | awk '{print $2}' | sort | uniq > $b
 comm -13 $a $b
+rm $a $b
